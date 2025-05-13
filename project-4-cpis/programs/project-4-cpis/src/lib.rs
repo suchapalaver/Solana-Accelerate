37f4be30a457de8371cab6c7ff6c151f6b627d42
Joseph Livesey <joseph@semiotic.ai>
2025-05-13 10:26:05 -0400

    chore(4-cpis): anchor init cpis project with rust test template

---
+++ project-4-cpis/programs/project-4-cpis/src/lib.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Eau8idrUiVQD3r9eEcqhxdhCZVdXAcxQQ4YWNFFnC42P");
+
+#[program]
+pub mod project_4_cpis {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", _ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    pub system_program: Program<'info, System>,
+}
